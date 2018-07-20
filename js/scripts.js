e0a2b9c824593c9c8e77fa6d1496e7bca07097aa
tonnymuchui <tonykanyingah@gmail.com>
2018-07-20 18:14:52 +0300

    add files

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,27 @@ function rollDice() {
 var status = document.getElementById("status");
 var p1 = Math.floor(Math.random() * 6) + 1;
 var pt1 = Math.floor(Math.random() *6) +p1;
+var hod = Math.floor(Math.random() *0) +0;
 var total1 = p1 + pt1 ;
-var p2 = Math.floor(Math.random() * 6) + 1;
-var pt2 = Math.floor(Math.random() * 6) + p2;
-var total2 = p2 + pt2;
 player1.innerHTML = p1 && pt1 ;
-status.innerHTML = "You rolled "+total1+".";
-
-// var total = p1 + p2;
-// player1.innerHTML = p1 ;
-// player2.innerHTML = p2 ;
-// status.innerHTML = "You rolled "+total+".";
-// if (p1 === p2) {
-//   status.innerHTML += "Doubles!!! you get a free turn";
-// };
+status.innerHTML = "You rolled"+p1+".";
+hold.innerHTML = total1;
+if (total1 > 15) {
+  hold.innerHTML += "win!!!!";
 };
-// function rollDice() {
-//   var player2 = document.getElementById("player2") ;
-//   var hold = document.getElementById("hold") ;
-//
-//
-//
-// };
+};
+function rollDice2() {
+  var player2 = document.getElementById("player2") ;
+  var hold = document.getElementById("hold") ;
+  var status = document.getElementById("status");
+  var p2 = Math.floor(Math.random() * 6) + 1;
+  var pt2 = Math.floor(Math.random() * 6) + p2;
+  var hod = Math.floor(Math.random() *0) +0;
+  var total2 = p2 + pt2;
+  player2.innerHTML = p2 && pt2 ;
+  status.innerHTML = "You rolled "+p2+".";
+  hold.innerHTML = total2;
+  if (total2 > 15) {
+    hold.innerHTML += "win!!"+".";
+  };
+};
