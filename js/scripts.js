cb86ce270c1d1f4c20c586d2f18f5ead1d16cfbe
tonnymuchui <tonykanyingah@gmail.com>
2018-07-20 14:29:17 +0300

    add js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function rollDice() {
+  var player1 = document.getElementById("player1") ;
+  var player2 = document.getElementById("player2") ;
+var status = document.getElementById("status");
+var p1 = Math.floor(Math.random() * 6) + 1;
+var p2 = Math.floor(Math.random() * 6) + 1;
+var total = p1 + p2;
+player1.innerHTML = p1 ;
+player2.innerHTML = p2 ;
+// status.innerHTML = "You rolled "+total+".";
+// if (p1 === p2) {
+//   status.innerHTML += "Doubles!!! you get a free turn";
+// };
+};
